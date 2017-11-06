c62a8ee23744a629d903512281d7c6d1075c13f6
Ben Lacy <ben.lacy@outlook.com>
2017-11-05 23:22:06 -0600

    adding weather-js

--- routes/routes.js
+++ routes/routes.js
@@ -6,23 +6,14 @@ var apitoken = "";
 module.exports = function (app) {
     // ----- INDEX.HTML ROUTE ----- //
     app.get('/', function(req, res) {
-        res.sendFile(path.join(__dirname, '/index.html'));
+        res.sendFile('/index.html');
     });
 
-    // ----- OPEN TRIVIA DB API CALL ----- //
+    // ----- LOCAL-WEATHER API CALL ----- //
     app.post('/api/choices', function(req, res) {
-        var choices = req.body;
-        var cat = choices.cat;
+        var zip = req.body;
         
-        if (!apitoken) {        // If no token has been generated, get one
-            var tokenRequestURL = "https://opentdb.com/api_token.php?command=request";
-            request(tokenRequestURL, function(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    var obj = JSON.parse(body);
-                    apitoken = obj.token;
-                }
-            });
-        }
+
         
         var triviaRequestURL = "https://opentdb.com/api.php?amount=10&category=" + cat + "&token=" + apitoken;
         request(triviaRequestURL, function(error, response, body) {
