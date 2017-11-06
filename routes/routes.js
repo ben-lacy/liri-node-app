2e51db5dbf7db7449a1cec23c5a134e711caa773
Ben Lacy <ben.lacy@outlook.com>
2017-11-06 00:13:34 -0600

    weather api call functional

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 var bodyParser = require("body-parser");
 var path = require("path");
-var request = require("request");
-var apitoken = "";
+var weather = require("weather-js");
 
 module.exports = function (app) {
     // ----- INDEX.HTML ROUTE ----- //
@@ -10,17 +9,16 @@ module.exports = function (app) {
     });
 
     // ----- LOCAL-WEATHER API CALL ----- //
-    app.post('/api/choices', function(req, res) {
-        var zip = req.body;
-        
-
-        
-        var triviaRequestURL = "https://opentdb.com/api.php?amount=10&category=" + cat + "&token=" + apitoken;
-        request(triviaRequestURL, function(error, response, body) {
-            if (!error && response.statusCode === 200) {
-                var obj = JSON.parse(body);
-                res.json(obj.results);
+    app.post('/api/location', function(req, res) {
+        var location = req.body.location;
+        weather.find({ search: location, degreeType: 'F' },
+            function (err, result) {
+                if(err){ throw err };
+                // console.log(JSON.stringify(result, null, 2));
+                for (var key in result) {
+                    console.log(result[key].current.observationpoint);
+                }
             }
-        });
+        )
     });
 }
